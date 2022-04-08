c15cceb629d0f27f578110476fe8cffc05fed76a
Chimoha31 <mokochii1108@gmail.com>
2022-04-08 14:18:05 -0700

    Found solution to fix

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,41 @@ import Icons from "./Icons";
 
 const Hero = () => {
   return (
-    <div>
+    <div className="svgs_container">
+      <style>
+        {`@keyframes animate {
+    0% {
+      transform: translateX(-10%);
+    }
+    33% {
+      transform: translateX(-30%);
+    }
+  66% {
+      transform: translateX(-10%);
+    }
+
+    100% {
+      transform: translateX(-30%);
+    }
+  }`}
+      </style>
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
+      <Icons />
       <Icons />
     </div>
   );
--- src/components/hero/Icons.jsx
+++ src/components/hero/Icons.jsx
@@ -1,30 +1,18 @@
 import React from "react";
 import Icon from "./Icon";
-// import './Icons.css';
+import './Icons.css';
 
 const Icons = () => {
   return (
-    <div className="hero_section">
-      <style>
-        {`@keyframes animate {
-        0% {
-          transform: translateX(0);
-        }
-        100% {
-          transform: translateX(-100%);
-        }
-      }`}
-      </style>
-      <div className="row">
-        <div
-          style={{
-            animation: `animate  60s linear forwards infinite`,
-          }}
-        >
-          <Icon />
-          <Icon />
-          <Icon />
-        </div>
+    <div className="row">
+      <div
+        style={{
+          animation: `animate  60s linear forwards infinite`,
+        }}
+      >
+        <Icon />
+        <Icon />
+        <Icon />
       </div>
     </div>
   );
