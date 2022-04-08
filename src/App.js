37183e7d3b8c6ff64382c8185c7ea22ac5801d79
Chimoha31 <mokochii1108@gmail.com>
2022-04-07 20:18:22 -0700

    Created components frame

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import "./App.css";
+import Hero from "./components/hero/Hero";
+import NavBar from "./components/navbar/NavBar";
+import AnimeList from "./components/animelist/AnimeList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
- 
+    <div>
+      <Hero />
+      <NavBar />
+      <AnimeList />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/animelist/AnimeList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AnimeList.css';
+
+const AnimeList = () => {
+  return (
+    <div>
+      <h1>AnimeList</h1>
+    </div>
+  )
+}
+
+export default AnimeList
