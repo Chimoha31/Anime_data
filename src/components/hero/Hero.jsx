47d75f520cd75307e05889d16eb4770f6c398e39
Chimoha31 <mokochii1108@gmail.com>
2022-04-08 12:27:16 -0700

    animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,8 +5,202 @@ import Icon from "./Icon";
 const Hero = () => {
   return (
     <div className="hero_section">
+      <style>
+        {`@keyframes animate {
+        0% {
+          transform: translateX(0);
+        }
+        100% {
+          transform: translateX(-100%);
+        }
+      }`}
+      </style>
       <div className="row">
-        <Icon />
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
+      <div className="row">
+        <div
+          style={{
+            animation: `animate  60s linear forwards linear infinite`,
+          }}
+        >
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
       </div>
     </div>
   );
