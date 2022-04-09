88a0cad5fb60d433bc82c9de22dab2cf3b1bcb32
Chimoha31 <mokochii1108@gmail.com>
2022-04-09 02:24:49 -0700

    Try to get data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Hero from "./components/hero/Hero";
-// import NavBar from "./components/navbar/NavBar";
 import AnimeList from "./components/animelist/AnimeList";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      {/* <NavBar /> */}
       <Router>
         <Routes>
           <Route exact path="/" element={<Hero />} />
--- src/components/animelist/AnimeList.jsx
+++ src/components/animelist/AnimeList.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import './AnimeList.css';
+import React, { useState } from "react";
+import NavBar from "../navbar/NavBar";
+import "./AnimeList.css";
 
 const AnimeList = () => {
+  const [search, setSearch] = useState("");
+
+  const getAnime = async (query) => {
+    const response = await fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`
+    ).then(res => res.json());
+    console.log(response.results);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getAnime(search);
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // const getAnime = async() => {
+  //   const response = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`);
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+  // getAnime();
+
   return (
     <div>
+      <NavBar />
       <h1>AnimeList</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={search} />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnimeList
+export default AnimeList;
