37232e987dee5f5427ec664a6637a195e341e9b8
Chimoha31 <mokochii1108@gmail.com>
2022-04-10 22:55:56 -0700

    Done TopAnime

--- src/components/animelist/topAnime/TopAnime.jsx
+++ src/components/animelist/topAnime/TopAnime.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./TopAnime.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const TopAnime = () => {
   const [topAnimeList, setTopAnimeList] = useState([]);
@@ -17,22 +20,62 @@ const TopAnime = () => {
     topAnime();
   }, []);
 
+  // slider
+  const settings = {
+    dots: false,
+    infinite: false,
+    centerPadding: "60px",
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="category_title">
       <h1 className="h1">Top Anime</h1>
       <div className="topanime_container">
-        {topAnimeList.map((topAnime, index) => (
-          <div key={index} className="eachAnime_container">
-            <a href={topAnime.url} target="_blank" rel="noreferrer">
-              <img src={topAnime.image_url} alt="anime img" />
-            </a>
-            <div className="anime_title">
-              <h2>{topAnime.title}</h2>
-              <p>Score:{topAnime.score} / 10</p>
-              <p>Episodes:{topAnime.episodes}</p>
+        <Slider {...settings}>
+          {topAnimeList.map((topAnime, index) => (
+            <div key={index} className="eachAnime_container">
+              <a href={topAnime.url} target="_blank" rel="noreferrer">
+                <img src={topAnime.image_url} alt="anime img" />
+              </a>
+              <div className="anime_title">
+                <h2>{topAnime.title}</h2>
+                <p>Rank: {topAnime.rank}</p>
+                <p>Score: {topAnime.score} / 10</p>
+                <p>Episodes: {topAnime.episodes}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
     </div>
   );
