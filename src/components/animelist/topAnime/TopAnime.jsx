f2fdddd8d73aea7f68a7bbff2fdb766368943601
Chimoha31 <mokochii1108@gmail.com>
2022-04-10 19:19:38 -0700

    styling

--- src/components/animelist/topAnime/TopAnime.jsx
+++ src/components/animelist/topAnime/TopAnime.jsx
@@ -1,27 +1,41 @@
-import React,{useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from "react";
+import "./TopAnime.css";
 
 const TopAnime = () => {
   const [topAnimeList, setTopAnimeList] = useState([]);
 
-    const topAnime = async() => {
-    const response = await fetch(`https://api.jikan.moe/v3/top/anime/1/bypopularity`);
+  const topAnime = async () => {
+    const response = await fetch(
+      `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+    );
     const data = await response.json();
     console.log(data.top);
     setTopAnimeList(data.top);
-  }
-  
+  };
+
   useEffect(() => {
     topAnime();
-  }, [])
+  }, []);
 
   return (
-    <div>
-      {topAnimeList.map((topAnime, index) =>(
-       <img src={topAnime.image_url} alt="anime img" key={index} />
-      ))}
+    <div className="category_title">
+      <h1 className="h1">Top Anime</h1>
+      <div className="topanime_container">
+        {topAnimeList.map((topAnime, index) => (
+          <div key={index} className="eachAnime_container">
+            <a href={topAnime.url} target="_blank" rel="noreferrer">
+              <img src={topAnime.image_url} alt="anime img" />
+            </a>
+            <div className="anime_title">
+              <h2>{topAnime.title}</h2>
+              <p>Score:{topAnime.score} / 10</p>
+              <p>Episodes:{topAnime.episodes}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopAnime
+export default TopAnime;
