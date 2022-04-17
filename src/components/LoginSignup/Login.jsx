f7d8dc69995ae45ca9a2757bb6b304c51660acfe
Chimoha31 <mokochii1108@gmail.com>
2022-04-17 14:35:22 -0700

    styling signup

--- src/components/LoginSignup/Login.jsx
+++ src/components/LoginSignup/Login.jsx
@@ -1,37 +1,40 @@
 import React from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
-import { Button, Form } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
+
 import GoogleButton from "react-google-button";
 
 const Login = () => {
   return (
-    <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Login</h2>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Email address" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="submit">
-              Log In
-            </Button>
+    <div className="login_container">
+      <Container style={{ width: "400px" }}>
+        <div className="p-4 box">
+          <h2 className="pb-3 text-center">Login</h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Email address" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="submit">
+                Log In
+              </Button>
+            </div>
+          </Form>
+          <hr />
+          <div>
+            <GoogleButton className="google_btn" />
           </div>
-        </Form>
-        <hr />
-        <div>
-          <GoogleButton className="google_btn" />
         </div>
-      </div>
 
-      <div className="p-4 mt-3 text-center box">
-        Don't have account yet? <Link to="/signup">Sign Up</Link>
-      </div>
-    </>
+        <div className="p-4 box mt-3 text-center">
+          Don't have account yet? <Link to="/signup">Sign Up</Link>
+        </div>
+      </Container>
+    </div>
   );
 };
 
