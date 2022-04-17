b2cd8b48a90eebaa376ba3b28092697e6e52c01e
Chimoha31 <mokochii1108@gmail.com>
2022-04-17 14:06:09 -0700

    login signup

--- src/components/LoginSignup/Signup.jsx
+++ src/components/LoginSignup/Signup.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import './Signup.css';
+import { Link } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
 
 const Signup = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="p-4 box">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Enter password" />
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <Button variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </div>
+        </Form>
 
-export default Signup
+        <div className="p-4 mt-3 text-center box">
+          Already have an account ? <Link to="/login">Log In</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
